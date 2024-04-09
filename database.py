c0291ad1fe19b8d026a9183f91a50d037bf412a0
Bonaventure <kakieosita@gmail.com>
2024-04-09 23:13:07 +0100

    dynamic pages

--- database.py
+++ database.py
@@ -11,9 +11,37 @@ connection_string = os.getenv('DB_CONNECTION_STRING')
 engine = create_engine(connection_string)
 
 
+# def load_job_from_db(id):
+#     with engine.connect() as conn:
+#         result = conn.execute(
+#             text("SELECT * FROM jobs WHERE id = :val"), {"val": id}
+#         )
+#         rows = result.fetchall()
+#
+#         # Get column names
+#         column_names = result.keys()
+#         job = []
+#         if len(rows) == 0:
+#             return None
+#         else:
+#             row_dict = dict(zip(column_names, rows[0]))
+#             job.append(row_dict)
+#             return job
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"), {"val": id}
+        )
+        row = result.fetchone()  # Fetch a single row
+
+        if row is None:
+            return None
+        else:
+            # Fetch column names
+            column_names = result.keys()
+            # Construct dictionary using column names and row values
+            job = dict(zip(column_names, row))
+            return job
 
 
-#
-# db_connection_string = "mysql+pymysql://avnadmin:AVNS_cDWv1klq_iGCO82-kAG@mysql-3488e08d-kakieosita-5890.a.aivencloud.com/defaultdb?charset=utf8mb4"
-#
-# engine = create_engine(db_connection_string)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 from database import engine
 from sqlalchemy import text
+from database import load_job_from_db
 
 app = Flask(__name__)
 
@@ -31,6 +32,12 @@ def hello_world():
 @app.route("/jobs")
 def list_jobs():
     return jsonify(load_jobs_from_db())
+
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    return render_template("jobpage.html", job=job)
 
 
 if __name__ == '__main__':
